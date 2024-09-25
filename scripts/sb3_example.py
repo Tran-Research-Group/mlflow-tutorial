c64ae358558a8f022ad20e94a847922586dec947
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-09-24 20:33:10 -0500

    ref: use mlflow-rl-tools

--- scripts/sb3_example.py
+++ scripts/sb3_example.py
@@ -1,36 +1,14 @@
+import os
 import sys
-from typing import Any
 
 import mlflow
-import numpy as np
+from mlflow_rl_tools.sb3.log import MLflowOutputFormat
+from mlflow_rl_tools.sb3.wrapper import ModelWrapper
 
 from stable_baselines3 import SAC
-from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
-
-
-class MLflowOutputFormat(KVWriter):
-    """
-    Dumps key/value pairs into MLflow's numeric format.
-    """
-
-    def write(
-        self,
-        key_values: dict[str, Any],
-        key_excluded: dict[str, str | tuple[str, ...]],
-        step: int = 0,
-    ) -> None:
-
-        for (key, value), (_, excluded) in zip(
-            sorted(key_values.items()), sorted(key_excluded.items())
-        ):
-
-            if excluded is not None and "mlflow" in excluded:
-                continue
-
-            if isinstance(value, np.ScalarType):
-                if not isinstance(value, str):
-                    mlflow.log_metric(key, value, step)
-
+from stable_baselines3.common.logger import HumanOutputFormat, Logger
+import imageio
+import gymnasium as gym
 
 total_timesteps: int = 100
 
@@ -44,15 +22,8 @@ loggers = Logger(
 )
 
 
-class ModelWrapper(mlflow.pyfunc.PythonModel):
-    def __init__(self, model):
-        self.model = model
+with mlflow.start_run(log_system_metrics=True) as run:
 
-    def predict(self, context, model_input):
-        return self.model.predict(model_input)
-
-
-with mlflow.start_run(log_system_metrics=True):
     mlflow.log_params({"total_timesteps": total_timesteps})
 
     model = SAC("MlpPolicy", "Pendulum-v1", verbose=2)
@@ -65,3 +36,19 @@ with mlflow.start_run(log_system_metrics=True):
         artifact_path="model",
         python_model=ModelWrapper(model),
     )
+
+    # Create animation
+    env = gym.make("Pendulum-v1", render_mode="rgb_array")
+    obs, _ = env.reset()
+    images = [env.render()]
+    for _ in range(100):
+        action, _ = model.predict(obs)
+        obs, _, terminated, truncated, _ = env.step(action)
+        images.append(env.render())
+        if terminated or truncated:
+            break
+
+    image_save_dir: str = os.path.join("tmp", run.info.run_id, "plots")
+    os.makedirs(image_save_dir, exist_ok=True)
+    imageio.mimsave(os.path.join(image_save_dir, "animation.gif"), images)
+    mlflow.log_artifact(os.path.join(image_save_dir, "animation.gif"), "plots")
--- scripts/sb3_pred_example.py
+++ scripts/sb3_pred_example.py
@@ -3,13 +3,14 @@ import numpy as np
 
 mlflow.set_tracking_uri("http://0.0.0.0:5000")
 
-logged_model = "runs:/36f4d7ea41ec40289cdabe9d10e57bb2/model"
+logged_model = "runs:/4125e0adeacf463ca1841cf7e42c9688/model"
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 
 print("Model loaded. The model object is:", loaded_model)
 
-loaded_model.predict(np.array([0.5, 0.5, 0.5]))
+action, _ = loaded_model.predict(np.array([0.5, 0.5, 0.5]))
+print("The model predicts the action:", action)
 
 pass
